6354dd6b2589e12dec31502ff507dcc58511118d
ojogal <ojog.al@gmail.com>
2023-01-06 16:08:28 +0200

    routes updated

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import sequelize from './db.js';
 import models from './models/models.js';
 import cors from 'cors';
+import router from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 app.get('/', (req, res) => {
   res.json('it works')
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import Router from 'express';
+import brandRouter from './brandRouter.js';
+import deviceRouter from './deviceRouter.js';
+import typeRouter from './typeRouter.js';
+import userRouter from './userRouter.js';
 
 const router = new Router();
 
-router.use('/user');
-router.use('/type');
-router.use('/brand');
-router.use('/device');
+router.use('/user', userRouter);
+router.use('/type', typeRouter);
+router.use('/brand', brandRouter);
+router.use('/device', deviceRouter);
 
 export default router
