90d0aae5a8c29244b767996e657dd3d1c6a8b22b
ojogal <ojog.al@gmail.com>
2023-01-07 20:40:58 +0200

    controllers updated

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,11 +1,20 @@
 import Device from '../models/models.js';
 import ApiError from '../error/apiError.js';
 import { UUID } from 'sequelize';
+import path from 'path';
 
 class DeviceController {
-  async index(req, res) {
-    const { name, price, brandId, typeId, info } = req.body;
-    const { img } = req.files;
+  async index(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = UUID.v4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+      return res.json(device)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
   };
 
   async show(req, res) {
