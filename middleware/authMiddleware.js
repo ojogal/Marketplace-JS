529861d3e2254b465ed333ba308a76d3b92cb0a1
ojogal <ojog.al@gmail.com>
2023-01-09 15:34:05 +0200

    authorization updated

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export default function (req, res, next) {
+  if (req.method == 'OPTIONS') {
+    next()
+  };
+
+  try {
+    const token = req.headers.authorization.split(' ')[1] // Bearer type
+
+    if (!token) {
+      return res.status(401).json({ message: 'Not authorised' })
+    };
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next()
+  } catch (e) {
+    res.status(401).json({ message: 'Not authorised' })
+  }
+};
