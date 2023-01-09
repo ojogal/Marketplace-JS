3a194df31399dcdbaa211a08d8425644507f8f1c
ojogal <ojog.al@gmail.com>
2023-01-09 13:54:00 +0200

    show method updated

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,23 +1,10 @@
-import Device from '../models/models.js';
+import { Device, DeviceInfo } from '../models/models.js';
 import ApiError from '../error/apiError.js';
 import { UUID } from 'sequelize';
 import path from 'path';
 
 class DeviceController {
   async index(req, res, next) {
-    try {
-      const { name, price, brandId, typeId, info } = req.body;
-      const { img } = req.files;
-      let fileName = UUID.v4() + '.jpg';
-      img.mv(path.resolve(__dirname, '..', 'static', fileName));
-      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
-      return res.json(device)
-    } catch (e) {
-      next(ApiError.badRequest(e.message))
-    }
-  };
-
-  async show(req, res) {
     let { brandId, typeId, limit, page } = req.query;
     page = page || 1;
     limit = limit || 9;
@@ -42,8 +29,38 @@ class DeviceController {
     return res.json(devices)
   };
 
-  async create(req, res) {
+  async show(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne(
+      {
+        where: { id },
+        include: [ { model: DeviceInfo, as: 'info' } ]
+      }
+    );
+    return res.json(device)
+  };
 
+  async create(req, res, next) {
+    try {
+      let { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = UUID.v4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+      if (info) {
+        info = JSON.parse(info);
+        info.forEach(i => DeviceInfo.create({
+          title: i.title,
+          description: i.description,
+          deviceId: device.id
+        }))
+      };
+
+      return res.json(device)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
   }
 };
 
